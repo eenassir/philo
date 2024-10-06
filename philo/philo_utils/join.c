97454cb863d557b25b311ca8c17b975a412caf40
El Mehdi Enassiri <eenassir@e3r5p7.1337.ma>
2024-10-06 22:38:38 +0100

    push

--- philo/philo_utils/join.c
+++ philo/philo_utils/join.c
@@ -6,7 +6,7 @@
 /*   By: eenassir <eenassir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/04 21:02:29 by eenassir          #+#    #+#             */
-/*   Updated: 2024/10/04 21:06:01 by eenassir         ###   ########.fr       */
+/*   Updated: 2024/10/06 22:37:41 by eenassir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,16 @@
 void join(pthread_t *th, t_init *init)
 {
 	int i;
-
+	
 	i = 0;
-	while (pthread_join(th[i++], NULL) == 0)
-		;
+	while (i < init->num_philo)
+	{
+		pthread_join(th[i], NULL);
+		i++;
+	}
+	pthread_mutex_destroy(&init->write_mutex);
+	pthread_mutex_destroy(&init->lock);
+	pthread_mutex_destroy(&init->time);
+	pthread_mutex_destroy(init->forks);
 	free (init->forks);
 }
